1cbde586cc8c8a7cfaf26af15296ec0d20e76e8d
narkhedek3 <narkhedek3@gmail.com>
2020-07-25 20:44:05 +0530

    header adjustment

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render = () => {
     return (
       <div className="row sticky-top header">
-        <div className="col p-2 m-2">
+        <div className="col p-2 pl-4">
           <h3>User Activities</h3>
         </div>
 
